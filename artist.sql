ea5560da024742fc7f6a136987c46d9adb601eb4
Carter Dooley <92332958+carterdooley@users.noreply.github.com>
2021-11-08 16:24:22 -0600

    Add files via upload

---
+++ artist.sql
@@ -0,0 +1,12 @@
+INSERT INTO artist (name)
+    VALUES ('Jessica'),
+    ('Leonides')
+    ('Blacksparrow')
+
+SELECT * FROM artist ORDER BY name desc LIMIT 10
+
+SELECT * FROM artist ORDER BY name asc LIMIT 5
+
+SELECT * FROM artist WHERE name LIKE 'Black%'
+
+SELECT * FROM artist WHERE name LIKE '%Black%'
---
+++ employee.sql
@@ -0,0 +1,7 @@
+SELECT first_name last_name FROM employee WHERE city = 'Calgary'
+
+SELECT Max(birth_date) FROM employee
+
+SELECT Min(birth_date) FROM employee
+
+SELECT * FROM employee WHERE reports_to = 'Nancy Edwards'
